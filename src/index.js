2b6a91de021a798279e0324306fff335eda01932
Shideravan <spannopan@yahoo.com.br>
2021-03-04 01:09:55 -0300

    Update em TODO

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
+// MELHORIAS FUTURAS
+// 1. Mostrar a localização de cada jogada no formato (col,row), para cada jogada no histórico.
+// 2. Estilizar com negrito o item da lista de jogadas que está selecionado no momento.
+// 3. Reescrever o componente Board para utilizar 2 loops para fazer os quadrados, em vez de deixá-los hardcoded.
+// 4. Adicionar um botão de toggle que lhe permita ordenar os jogadas em ordem ascendente ou descendente.
+// 5. Quando alguém ganhar, destaque os 3 quadrados que causaram a vitória.
+// 6. Quando ninguém ganhar, exiba uma mensagem informando que o resultado foi um empate.
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
